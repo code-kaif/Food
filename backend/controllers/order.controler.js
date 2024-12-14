ea8e267e88aebed870977041c057203be6d63761
Kaif <142976945+code-kaif@users.noreply.github.com>
2024-12-14 16:21:42 +0530

    Update order.controler.js

--- backend/controllers/order.controler.js
+++ backend/controllers/order.controler.js
@@ -6,7 +6,7 @@ import "dotenv/config";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5174";
+  const frontend_url = "https://parmal-frontned.onrender.com";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
